ae23c9cabda3563584b939b327fc4c7d33a5e8ad
somsak <somsak.kaeworasan@outlook.com>
2021-05-10 01:44:01 +0700

    First commit.

---
+++ QRCodeScanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QRCodeScanner
+//
+//  Created by somsak on 10/5/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
